8132877336fc68ab6640108d34fa1e855d53047c
Vitor Scheffler <160888109+VitorScheffler@users.noreply.github.com>
2025-04-14 16:35:18 +0000

    Projeto inicial

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Exemplo de interação com o formulário
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulário enviado com sucesso!');
+  });
+  
